a6c918156a049e84465f9c44c45d253e04f8e0bb
QMaire <maire.quentin@orange.fr>
2021-08-03 17:35:19 +0200

    Prise en compte derniere remarque

--- main_convert_parlementary_file_with_new_comment.py
+++ main_convert_parlementary_file_with_new_comment.py
@@ -11,7 +11,7 @@ def main():
     months = ('Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 
                 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre')
     today_str = f"{months[today.month-1]}_{today.year}"
-    name_zip = 'Fiche_Parlementaire_Apres_Commentaires_{}.zip'.format(today_str)
+    name_zip = 'parlementary_file_with_new_comment{}.zip'.format(today_str)
     f=zipfile.ZipFile(name_zip,'w',zipfile.ZIP_DEFLATED)
     f.write("reports_pdf")
     f.write("modified_reports")
